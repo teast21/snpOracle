dffbcb816e93be61ce02df50c1338f6e3457a8ab
teast21 <teastman@foundrydigital.com>
2024-02-23 15:54:17 +0000

    adjusting requirements.txt to be less specific, testnet eco files

--- validator.config.js
+++ validator.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'test-validator',
       script: 'python3',
-      args: './neurons/validator.py --wallet.name testValidator --wallet.hotkey default --netuid 1 --logging.debug --logging.trace --subtensor.network local --subtensor.chain_endpoint 127.0.0.1:9946'
+      args: './neurons/validator.py --wallet.name testValidator --wallet.hotkey default --netuid 93 --logging.debug --logging.trace --subtensor.network test'
     },
   ],
 };
