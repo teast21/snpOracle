ba3cac04a7be2ec5e387381627bb06ecbe4d8aaa
theBom <109384972+teast21@users.noreply.github.com>
2024-02-25 18:24:15 -0500

    wallet should default to default :-)

--- validator.config.js
+++ validator.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'validator',
       script: 'python3',
-      args: './neurons/validator.py --wallet.name <WALLET_NAME> --wallet.hotkey <HOTKEY_NAME> --netuid 93 --logging.debug --logging.trace --subtensor.network finney'
+      args: './neurons/validator.py --netuid 93 --logging.debug --logging.trace --subtensor.network finney'
     },
   ],
 };
