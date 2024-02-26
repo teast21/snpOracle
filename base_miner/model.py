bff29cbb6b0116a5e46debfdd3879b9a30abc602
Rahul Arulkumaran <rarulkumaran@foundrydigital.com>
2024-02-26 10:37:03 -0600

    add regression base miner

--- base_miner/model.py
+++ base_miner/model.py
@@ -2,14 +2,16 @@
 # Copyright © 2023 Foundry Digital
 
 # Import necessary modules to use for model creation - can be downloaded using pip
+import joblib
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 
-def create_and_save_base_model(scaler:MinMaxScaler, X_scaled:np.ndarray, y_scaled:np.ndarray) -> float:
+def create_and_save_base_model_lstm(scaler:MinMaxScaler, X_scaled:np.ndarray, y_scaled:np.ndarray) -> float:
     """
     Base model that can be created for predicting the S&P 500 close price
 
@@ -63,6 +65,59 @@ def create_and_save_base_model(scaler:MinMaxScaler, X_scaled:np.ndarray, y_scale
 
     # Rescale back to original range
     predicted_prices = scaler.inverse_transform(predicted_prices)
+    y_test_rescaled = scaler.inverse_transform(y_test.reshape(-1, 1))
+
+    # Evaluate
+    mse = mean_squared_error(y_test_rescaled, predicted_prices)
+    print(f'Mean Squared Error: {mse}')
+    
+    return mse
+
+def create_and_save_base_model_regression(scaler:MinMaxScaler, X_scaled:np.ndarray, y_scaled:np.ndarray) -> float:
+    """
+    Base model that can be created for predicting the S&P 500 close price
+
+    The function creates a base model, given a scaler, inputs and outputs, and
+    stores the model weights as a .h5 file in the mining_models/ folder. The model
+    architecture and model name given now is a placeholder, can (and should)
+    be changed by miners to build more robust models.
+
+    Input:
+        :param scaler: The scaler used to scale the inputs during model training process
+        :type scaler: sklearn.preprocessing.MinMaxScaler
+
+        :param X_scaled: The already scaled input data that will be used by the model to train and test
+        :type X_scaled: np.ndarray
+
+        :param y_scaled: The already scaled output data that will be used by the model to train and test
+        :type y_scaled: np.ndarray
+    
+    Output:
+        :returns: The MSE of the model on the test data
+        :rtype: float
+    """
+    model_name = "mining_models/base_linear_regression"
+
+    # Split data into training and testing
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_scaled, test_size=0.2, random_state=42)
+
+    # LSTM model - all hyperparameters are baseline params - should be changed according to your required
+    # architecture. LSTMs are also not the only way to do this, can be done using any algo deemed fit by
+    # the creators of the miner.
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    '''with h5py.File(f'{model_name}.h5', 'w') as hf:
+        hf.create_dataset('coefficients', data=model.coef_)
+        hf.create_dataset('intercept', data=model.intercept_)'''
+    joblib.dump(model, f"{model_name}.joblib")
+
+    # Predict the prices - this is just for a local test, this prediction just allows
+    # miners to assess the performance of their models on real data.
+    predicted_prices = model.predict(X_test)
+
+    # Rescale back to original range
+    predicted_prices = scaler.inverse_transform(predicted_prices.reshape(-1, 1))
     y_test_rescaled = scaler.inverse_transform(y_test.reshape(-1, 1))
 
     # Evaluate
--- base_miner/predict.py
+++ base_miner/predict.py
@@ -3,18 +3,19 @@
 
 # Import modules that already exist or can be installed using pip
 from datetime import datetime
+import joblib
 import numpy as np
 import pandas as pd
 from pytz import timezone
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import load_model
-import yfinance as yf
+from model import create_and_save_base_model_lstm, create_and_save_base_model_regression
 
 # import custom defined files
-from base_miner.get_data import prep_data, scale_data, round_down_time
+from get_data import prep_data, scale_data, round_down_time
 
 
-def predict(timestamp:datetime, scaler:MinMaxScaler, model) -> float:
+def predict(timestamp:datetime, scaler:MinMaxScaler, model, type) -> float:
     """
     Predicts the close price of the next 5m interval
 
@@ -59,29 +60,33 @@ def predict(timestamp:datetime, scaler:MinMaxScaler, model) -> float:
     # data.to_csv('mining_models/base_miner_data.csv')
     input = matching_row[['Open', 'High', 'Low', 'Volume', 'SMA_50', 'SMA_200', 'RSI', 'CCI', 'Momentum']]
 
-    input = np.array(input, dtype=np.float32).reshape(1,-1)
-    input = np.reshape(input, (1,1, input.shape[1]))
-    print(input)
+    if(type!='regression'):
+        input = np.array(input, dtype=np.float32).reshape(1,-1)
+        input = np.reshape(input, (1,1, input.shape[1]))
+        print(input)
 
     prediction = model.predict(input)
-    prediction = scaler.inverse_transform(prediction)
+    if(type!='regression'):
+        prediction = scaler.inverse_transform(prediction.reshape(1,-1))
 
     return prediction
 
 # Uncomment this section if you wanna do a local test without having to run the miner
 # on a subnet. This main block (kinda) mimics the actual validator response being sent
-'''if(__name__=='__main__'):
+if(__name__=='__main__'):
     data = prep_data()
     scaler, X, y = scale_data(data)
+    #mse = create_and_save_base_model_regression(scaler, X, y)
+
+    #model = joblib.load('mining_models/base_linear_regression.joblib')
     model = load_model('mining_models/base_lstm.h5')
     ny_timezone = timezone('America/New_York')
     current_time_ny = datetime.now(ny_timezone)
     timestamp = current_time_ny.isoformat()
 
-
     #mse = create_and_save_base_model(scaler, X, y)
-    prediction = predict(timestamp, scaler, model)
-    print(prediction)'''
+    prediction = predict(timestamp, scaler, model, type='lstm')
+    print(prediction)
     
 
 
