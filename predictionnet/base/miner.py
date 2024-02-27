0a3d63d82862cc6b2cab9a097e514aa234975212
Zach Ross <zross@foundrydigital.com>
2024-02-27 11:39:18 -0500

    revert base miner weight setting trait

--- predictionnet/base/miner.py
+++ predictionnet/base/miner.py
@@ -183,30 +183,29 @@ class BaseMinerNeuron(BaseNeuron):
         Raises:
             Exception: If there's an error while setting weights, the exception is logged for diagnosis.
         """
-        # try:
-        #     # --- query the chain for the most current number of peers on the network
-        #     chain_weights = torch.zeros(
-        #         self.subtensor.subnetwork_n(netuid=self.metagraph.netuid)
-        #     )
-        #     chain_weights[self.uid] = 1
+        try:
+            # --- query the chain for the most current number of peers on the network
+            chain_weights = torch.zeros(
+                self.subtensor.subnetwork_n(netuid=self.metagraph.netuid)
+            )
+            chain_weights[self.uid] = 1
 
-        #     # --- Set weights.
-        #     self.subtensor.set_weights(
-        #         wallet=self.wallet,
-        #         netuid=self.metagraph.netuid,
-        #         uids=torch.arange(0, len(chain_weights)),
-        #         weights=chain_weights.to("cpu"),
-        #         wait_for_inclusion=False,
-        #         version_key=self.spec_version,
-        #     )
+            # --- Set weights.
+            self.subtensor.set_weights(
+                wallet=self.wallet,
+                netuid=self.metagraph.netuid,
+                uids=torch.arange(0, len(chain_weights)),
+                weights=chain_weights.to("cpu"),
+                wait_for_inclusion=False,
+                version_key=self.spec_version,
+            )
 
-        # except Exception as e:
-        #     bt.logging.error(
-        #         f"Failed to set weights on chain with exception: { e }"
-        #     )
+        except Exception as e:
+            bt.logging.error(
+                f"Failed to set weights on chain with exception: { e }"
+            )
 
-        # bt.logging.info(f"Set weights: {chain_weights}")
-        pass
+        bt.logging.info(f"Set weights: {chain_weights}")
 
     def resync_metagraph(self):
         """Resyncs the metagraph and updates the hotkeys and moving averages based on the new metagraph."""
